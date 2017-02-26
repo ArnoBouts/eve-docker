0d0df9b300856535952580240e380cf153a03eff
Eve <eve@no-cloud.fr>
2017-02-26 23:41:15 +0100

    ctrl M useless

--- eve.sh
+++ eve.sh
@@ -2,7 +2,7 @@
 
 BEARER=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${PLUGIN_BASE_IMAGE}:pull" | cut -d'"' -f4)
 
-SHA=$(curl -s -I -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -H "Authorization: Bearer ${BEARER}" -X GET "https://registry.hub.docker.com/v2/${PLUGIN_BASE_IMAGE}/manifests/${PLUGIN_TAG}" | grep "Docker-Content-Digest" | cut -d" " -f2)
+SHA=$(curl -s -I -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -H "Authorization: Bearer ${BEARER}" -X GET "https://registry.hub.docker.com/v2/${PLUGIN_BASE_IMAGE}/manifests/${PLUGIN_TAG}" | grep "Docker-Content-Digest" | grep -o "sha256:[0-9a-fA-F]*")
 
 cd eve
 

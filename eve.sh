8732ed098d4f156696bc982ad31043d664dbfd14
Eve <eve@no-cloud.fr>
2017-03-24 17:52:55 +0100

    add log

--- eve.sh
+++ eve.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Get Docker image digest for ${PLUGIN_BASE_IMAGE}:${PLUGIN_TAG}"
+
 BEARER=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${PLUGIN_BASE_IMAGE}:pull" | cut -d'"' -f4)
 
 SHA=$(curl -s -I -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -H "Authorization: Bearer ${BEARER}" -X GET "https://registry.hub.docker.com/v2/${PLUGIN_BASE_IMAGE}/manifests/${PLUGIN_TAG}" | grep "Docker-Content-Digest" | grep -o "sha256:[0-9a-fA-F]*")
